bcc24cae327bd1f8d6c42b6ccaf182c55a0f6557
AnushkaPraveen <anushkapraveen98@gmail.com>
2020-11-18 03:34:07 +0530

    create setting file

---
+++ src/component/Settings/Settings.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Settings extends Component {
+render(){
+    return(<div>
+        <h1>Hello admin!</h1>
+    </div>)
+}
+}
+
+export default Settings;
