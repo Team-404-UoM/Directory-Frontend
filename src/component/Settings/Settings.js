dd177adddbbab52a0b8bd43658722d864a8ddf1e
AnushkaPraveen <anushkapraveen98@gmail.com>
2020-11-18 03:35:39 +0530

    change h1 tag

--- src/component/Settings/Settings.js
+++ src/component/Settings/Settings.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Settings extends Component {
 render(){
     return(<div>
-        <h1>Hello admin!</h1>
+        <h1>Hello world!</h1>
     </div>)
 }
 }
