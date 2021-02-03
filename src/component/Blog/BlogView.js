9f98872928ccb3aecfefed274bc6a199fd25f0ad
AnushkaPraveen <anushkapraveen98@gmail.com>
2021-02-04 03:46:37 +0530

    created time,updated time,edit mode categorie,

--- src/component/Blog/BlogView.js
+++ src/component/Blog/BlogView.js
@@ -3,6 +3,7 @@ import { Component } from 'react';
 import axios from 'axios';
 import pic2 from '../Forum/pic2.jpg';
 import './Blogview.css';
+import moment from 'moment';
 
 
 
@@ -13,7 +14,9 @@ class BlogView extends Component {
             title:"",
             image:"",
             body:"",
-            categorie:""
+            categorie:"",
+            createtime:"",
+            updatetime:"",
         };
     }
 
@@ -24,7 +27,9 @@ class BlogView extends Component {
                 title: res.data.blog.title,
                 image: res.data.blog.image,
                 body: res.data.blog.body,
-                categorie: res.data.blog.categorie
+                categorie: res.data.blog.categorie,
+                createtime:res.data.blog.createdAt,
+                updatetime:res.data.blog.updatedAt,
             }))
         console.log(this.state.title)
         //.catch((err)=>console.log(err))
@@ -66,8 +71,8 @@ class BlogView extends Component {
                 <React.Fragment>
                 
                     <h1 style={{ textAlign: 'center', marginTop: '20px' }}>{this.state.title}</h1>
-                    <p style={{fontSize:'13px',textAlign:"center"}}><img src={pic2} alt="" style={{width:'30px',height:'30px',marginRight:'5px',marginTop:'-5px',marginLeft:'5px',borderRadius:'2px'}}/><strong>Anushka Praveen </strong><span style={{marginLeft:"40px"}}>create at:Jan 08 2020</span><span style={{marginLeft:"50px"}}>update at:Jan 16 2020</span></p>
-                    <p><span style={{textAlign:"center"}}>Categorie | {this.state.categorie}</span></p>
+                    <p style={{fontSize:'13px',textAlign:"center"}}><img src={pic2} alt="" style={{width:'30px',height:'30px',marginRight:'5px',marginTop:'-5px',marginLeft:'5px',borderRadius:'2px'}}/><strong>Anushka Praveen </strong><span style={{marginLeft:"40px"}}><strong>Created At: {moment(this.state.createtime).format('MMM DD ,YYYY')}</strong></span><span style={{marginLeft:"50px"}}><strong>Updated At: {moment(this.state.updatetime).format('MMM DD ,YYYY')}</strong></span></p>
+                    <p><span className="categorie-area">Categorie | {this.state.categorie}</span></p>
                   <img src={this.state.image} alt='Cover' style={{border:'1px solid gray',width:'29.8rem',height:'20rem',marginLeft:'35%',marginBottom:'30px',borderRadius:'5px'}}/> 
         <p style={{ padding: '20px 50px 20px' }}dangerouslySetInnerHTML={{__html:this.state.body}}></p>
                 </React.Fragment>
