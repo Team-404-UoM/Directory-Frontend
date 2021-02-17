f920ef5ade6adc716568ec4d11527c6cba4929b0
AnushkaPraveen <anushkapraveen98@gmail.com>
2021-02-18 04:00:45 +0530

    blog style chnages

--- src/component/Navbar/HomeNavbar.js
+++ src/component/Navbar/HomeNavbar.js
@@ -1,50 +1,36 @@
-import React, { Component } from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import logo from './Tream404.jpg';
-import './Nav.css';
+import React,{Component} from 'react';
+import {Navbar,Nav} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
+import logo from './Team404.jpg';
+import './Nav.css'; 
 
-class HomeNavbar extends Component {
-    render() {
-        return ( < div >
-            <
-            Navbar bg = "dark"
-            variant = "dark" >
-            <
-            Navbar.Brand href = "#home" >
-            <
-            img alt = ""
-            src = { logo }
-            width = "50"
-            height = "50"
-            className = "d-inline-block align-top" /
-            > { ' ' }
+class HomeNavbar extends Component{
+    render(){
+        return(<div>
+<Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">
+      <img
+        alt=""
+        src={logo}
+        width="50"
+        height="50"
+        className="d-inline-block align-top"
+      />{' '}
+    
+    </Navbar.Brand>
+    <Nav className="mr-auto navigations">
+      <Nav.Link as={Link} to='/'>Home</Nav.Link>
+      <Nav.Link as={Link} to="../AboutUs">About</Nav.Link>
+      <Nav.Link as={Link} to="../Events" >Event</Nav.Link>
+      <Nav.Link as={Link} to="../Gallery">Gallery</Nav.Link>
+      <Nav.Link as={Link} to="../Blog" >Blog</Nav.Link>
+      <Nav.Link as={Link} to="../Forum" >Forum</Nav.Link>
+      <Nav.Link as={Link} to="../Test" >Test</Nav.Link>
+      <Nav.Link as={Link} to="../Settings" >Settings</Nav.Link>
+    </Nav>
+  </Navbar>
 
-            <
-            /Navbar.Brand> <
-            Nav className = "mr-auto navigations" >
-            <
-            Nav.Link as = { Link }
-            to = '/' > Home < /Nav.Link> <
-            Nav.Link as = { Link }
-            to = "../AboutUs" > About < /Nav.Link> <
-            Nav.Link as = { Link }
-            to = "../Events" > Event < /Nav.Link> <
-            Nav.Link as = { Link }
-            to = "../Gallery" > Gallery < /Nav.Link> <
-            Nav.Link as = { Link }
-            to = "../Blog" > Blog < /Nav.Link> <
-            Nav.Link as = { Link }
-            to = "../Forum" > Forum < /Nav.Link> <
-            Nav.Link as = { Link }
-            to = "../Test" > Test < /Nav.Link> <
-            Nav.Link as = { Link }
-            to = "../Settings" > Settings < /Nav.Link> <
-            /Nav> <
-            /Navbar>
-
-            <
-            /div>
+  </div>
         )
     }
 }
