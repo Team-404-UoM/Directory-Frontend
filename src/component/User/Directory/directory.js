0eeaafc5644751c900b3c49cfbaa976009ce6679
vimeshi <vimeshi.bhagya@gmail.com>
2021-03-26 11:51:48 +0530

    directory

--- src/component/User/Directory/directory.js
+++ src/component/User/Directory/directory.js
@@ -1,124 +1,79 @@
 import React, { Component } from "react";
 import './directory.css';
-
-
+import pic2 from "./pic2.jpg";
+import pic1 from "./pic1.jpg";
+import pic3 from "./pic3.jpg";
+import axios from "axios";
 
 
 class directory extends Component{
+    constructor(props) {
+        super(props);
+    this.state={
+        allprofile:[],
+
+    }
+    }
+
+
+    componentDidMount(){
+        this.getProfile();
+        
+    }
+
+    getProfile(){
+        axios.get('http://localhost:4000/home')
+        .then((res)=>this.setState({allprofile:res.data.reverse()}))
+      
+    }
+
     render(){
       return (
         <div>
-          <title>Directory website </title>
-          <link rel="stylesheet" href="style.css" />
-          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
-          <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" />
-          {/*search bar */}
-          <ul className="nav">
-            <li id="settings">
-            </li>
-            <li>
-              <a href="#">Application</a>
-            </li>
-            <li>
-              <a href="#">Board</a>
-            </li>
-            <li id="search">
-              <form action method="get">
-                <input type="text" name="search_text" id="search_text" placeholder="Search" />
-                <input type="button" name="search_button" id="search_button" />
-              </form>
-            </li>
-            <li id="options">
-              <a href="#">Options</a>
-              <ul className="subnav">
-                <li><a href="#">Settings</a></li>
-                <li><a href="#">Application</a></li>
-                <li><a href="#">Board</a></li>
-                <li><a href="#">Options</a></li>
-              </ul>
-            </li>
-          </ul>
-          {/*user profiles */}
-          <section className="users-feedback">
-            <h1>WEB MEMBER DIRECTORY</h1>
-            <div className="container">
-              <div className="row">
-                <div className="col-md-4">
-                  <div className="user-review">
-                    <p> </p><div>Faculty of Business </div>
-                    <div>Batch 14</div>
-                    <div>Manager at Commercial Credit PLC</div>
-                    <p />
-                    <h5>Nirasha Wimalasooriya</h5>
-                    <small>Colombo</small>
-                  </div>
-                  <img src="images/user1.jpeg" />
+         <h1>Web Member Directory</h1>
+         <div class ="container">
+        <div class ="row">
+            {this.state.allprofile.map((profile)=>(
+            <div class="col-md-4">
+                <div class="user-review">
+                    <p> <div>Faculty of {profile.faculty} </div>
+                        <div>Batch {profile.Batch}</div>
+                        <div>Manager at Commercial Credit PLC</div>
+                    </p>
+                        <h5>{profile.firstName} {profile.lastName}</h5>
+                        <small>Colombo</small>
                 </div>
-                <div className="col-md-4">
-                  <div className="user-review">
-                    <p> </p><div>Faculty of Information Technology </div>
-                    <div>Batch 15</div>
-                    <div>Manager at Commercial Credit PLC</div>
-                    <p />
-                    <h5>Vimeshi Bhagya</h5>
-                    <small>Kandy</small>
-                  </div>
-                  <img src="images/user2.jpg" />
+                <img className="profile-img" src ={pic2}/>
+            </div>))}
+
+
+            <div class="col-md-4">
+                <div class="user-review">
+                    <p> <div>Faculty of Business </div>
+                        <div>Batch 14</div>
+                        <div>Manager at Commercial Credit PLC</div>
+                    </p>
+                        <h5>Nirasha Wimalasooriya</h5>
+                        <small>Colombo</small>
                 </div>
-                <div className="col-md-4">
-                  <div className="user-review">
-                    <p> </p><div>Faculty of Engineering</div>
-                    <div>Batch 15</div>
-                    <div>Manager at Commercial Credit PLC</div>
-                    <p />
-                    <h5>Ramodya Perea</h5>
-                    <small>Kurunegala</small>
-                  </div>
-                  <img src="images/user3.jpg" />
-                </div>
-              </div>
+                <img className="profile-img" src ={pic1}/>
             </div>
-            {/*</section>*/}
-            {/*<section class="users-feedback"> */}
-            <div className="container">
-              <div className="row">
-                <div className="col-md-4">
-                  <div className="user-review">
-                    <p> </p><div>Faculty of Business </div>
-                    <div>Batch 14</div>
-                    <div>Manager at Commercial Credit PLC</div>
-                    <p />
-                    <h5>Nirasha Wimalasooriya</h5>
-                    <small>Colombo</small>
-                  </div>
-                  <img src="images/user1.jpeg" />
+            <div class="col-md-4">
+                <div class="user-review">
+                    <p> <div>Faculty of Business </div>
+                        <div>Batch 14</div>
+                        <div>Manager at Commercial Credit PLC</div>
+                    </p>
+                        <h5>Ramodya Lakrandi</h5>
+                        <small>Colombo</small>
                 </div>
-                <div className="col-md-4">
-                  <div className="user-review">
-                    <p> </p><div>Faculty of Information Technology </div>
-                    <div>Batch 15</div>
-                    <div>Manager at Commercial Credit PLC</div>
-                    <p />
-                    <h5>Vimeshi Bhagya</h5>
-                    <small>Kandy</small>
-                  </div>
-                  <img src="images/user2.jpg" />
-                </div>
-                <div className="col-md-4">
-                  <div className="user-review">
-                    <p> </p><div>Faculty of Engineering</div>
-                    <div>Batch 15</div>
-                    <div>Manager at Commercial Credit PLC</div>
-                    <p />
-                    <h5>Ramodya Perea</h5>
-                    <small>Kurunegala</small>
-                  </div>
-                  <img src="images/user3.jpg" />
-                </div>
-              </div>
+                <img className="profile-img" src ={pic3}/>
             </div>
-          </section>
+            </div>
+            </div>
+           
+
+         
         </div>
     
       );
