5bdaeab9fdb64ae9e9a1dff32fa4a64ed1b580a6
AnushkaPraveen <anushkapraveen98@gmail.com>
2021-04-05 00:35:25 +0530

    profileedit

--- src/component/Navbar/HomeNavbar.js
+++ src/component/Navbar/HomeNavbar.js
@@ -66,6 +66,7 @@ islogged:"",
    
    <NavDropdown title="User Name">
      <NavDropdown.Item onClick={() => { logout() }}>Logout</NavDropdown.Item>
+     <NavDropdown.Item as={Link} to="../ProfileEdit">Profile</NavDropdown.Item>
    </NavDropdown>
   </Navbar>
   
